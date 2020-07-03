2cc17fa528fe449af5b1cf423e2b380f21f93e47
Gogi Loloyan <fiftyone157@gmail.com>
2020-07-04 02:29:35 +0400

    Updates

---
+++ precache-manifest.e06dda0f77dbd6b2b845da61941bf8f6.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "73e31e886b740a9a3efd6c405f4e13da",
+    "url": "/carental/index.html"
+  },
+  {
+    "revision": "bccf8a3ee323a716ee7e",
+    "url": "/carental/static/css/main.35ad2e0c.chunk.css"
+  },
+  {
+    "revision": "cc4cad66cd4e0e19c219",
+    "url": "/carental/static/js/2.98925619.chunk.js"
+  },
+  {
+    "revision": "64f7e58d0b20d4d7a9a6188b47824605",
+    "url": "/carental/static/js/2.98925619.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "bccf8a3ee323a716ee7e",
+    "url": "/carental/static/js/main.ddafde81.chunk.js"
+  },
+  {
+    "revision": "635f52d8467c72087f3f",
+    "url": "/carental/static/js/runtime-main.d8fa3db9.js"
+  },
+  {
+    "revision": "5938fcdc82ce31a3dc99772af8fb8c1b",
+    "url": "/carental/static/media/services_car.5938fcdc.png"
+  }
+]);
